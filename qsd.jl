2cad90c763e6056ba415485881572c63ace8a7ff
miftahhadi <majrur.mansub@gmail.com>
2025-12-29 12:51:43 +0300

    Initial commit

---
+++ qsd.jl
@@ -0,0 +1,213 @@
+using LinearAlgebra, Random, Statistics, SparseArrays
+using Base.Threads, Distributed
+
+include("utils.jl")
+
+# Main QSD function
+function runQSD(
+    L::Int,
+    exp_H,
+    diag_block,
+    VMaj,
+    idty2L,
+    majorana_indices,
+    gamma::Float64,
+    t_max::Float64,
+    dt::Float64,
+    itraj=nothing,
+    rng=Random.GLOBAL_RNG
+)
+    # Prepares the parameters
+    nsteps = round(Int, t_max / dt)
+
+    start_step = 1
+
+    # Prepare G0 and U0
+    G = GInit(L)
+    U = buildU0(L)
+
+    # Preallocate containers. 
+    times = zeros(Float64, nsteps)
+    entropies_arr = zeros(Float64, nsteps)
+    dξ = zeros(Float64, L)
+    n_i = zeros(Float64, L)
+
+    # Temporary containers for computations
+    temp1 = similar(U) # can hold exp_H * U0
+    temp2 = similar(U) # can hold exp_dT * temp1
+
+    dT_vec = zeros(Float64, L)
+    exp_dTplus = zeros(Float64, L)
+    exp_dTminus = zeros(Float64, L)
+
+    # Let's start the time evolution
+    for step in start_step:nsteps 
+        # What is current time?
+        current_time = step * dt
+        # Do we compute the entanglement entropy at this step?
+        compute_Sent = isComputeS(step, dt)
+
+        # Sample Wiener increment
+        # dξ = sqrt(gamma * dt) * randn(rng, L)
+        randn!(rng, dξ) # dξ ~ N(0,1)
+        @inbounds for i in 1:L
+            dξ[i] *= sqrt(gamma * dt) 
+        end
+
+        # Get the occupation number <n_i> = <c_i† c_i> from G
+        # In G, it is block G[1:L, 1:L]
+        # We use G from the previous step
+        @inbounds for i in 1:L
+            n_i[i] = real(G[i, i])
+        end
+
+        # Noise matrix
+        @inbounds for i in 1:L 
+            dT_vec[i] = 0.5 * (dξ[i] + gamma*dt * (2*n_i[i] - 1.0))
+        end         
+
+        @inbounds for i in 1:L
+            v = 2.0 * dT_vec[i]
+            exp_dTplus[i] = exp(v)
+            exp_dTminus[i] = exp(-v)
+        end
+
+        # Begin calculating U_tilde
+        mul!(temp1, exp_H, U) # temp1 = exp_H * U
+
+        @inbounds for j in 1:L 
+            @views temp2[j, :] .= exp_dTminus .* temp1[j, :]
+            @views temp2[L + j, :] .= exp_dTplus .* temp1[L + j, :]
+        end
+
+        Utilde = temp2
+
+        # QR decomposition
+        Q, R = qr(U_tilde)
+        U .= Matrix(Q)
+
+        # Compute new G for current time 
+        mul!(temp1, U, diag_block)  # temp1 = U * diag_block
+        mul!(G, temp1, U')          # G = temp1 * U' 
+
+        # Enforce the hermiticity of G due to numerical errors
+        adjoint!(temp2, G)  # temp2 = G†
+
+        G .+= temp2     # G = G + G†
+        G .*= 0.5       # G = 0.5 * (G + G†)
+
+        if compute_Sent
+            # Compute the entanglement entropy
+            S_ent = getEntanglementEntropy(G, idty2L, VMaj, majorana_indices)
+
+            # Store the results
+            times[step] = current_time
+            entropies_arr[step] = S_ent
+
+            if itraj !== nothing
+                println("Trajectory #$(itraj)")                
+            end
+            println("Time: $(current_time) / $(t_max)\nS_ent = $(S_ent)\n")
+        end
+    
+        if itraj !==nothing
+            println("Trajectory #$(itraj) completed at $(Dates.format(now(), "yyyy-mm-dd HH:MM:SS"))")
+        end
+
+        return times, entropy_arr    
+        
+    end
+
+
+end
+
+# Multi-threaded calculation of QSD ensemble
+function runQSDEnsembleParallel(
+    L::Int,
+    ntraj::Int,
+    exp_H,
+    diag_block,
+    VMaj,
+    idty2L,
+    majorana_indices,
+    gamma::Float64,
+    t_max::Float64,
+    dt::Float64,
+    base_seed::Int=1234
+)
+    nsteps = round(Int, t_max / dt)
+
+    # Containers
+    S_trajs = zeros(Float64, nsteps, ntraj)
+    times = collect(1:nsteps) .* dt
+
+    @threads for j in 1:ntraj 
+        seed = base_seed + j
+        rng = Xoshiro(seed)
+        t, S = runQSD(
+            L,
+            exp_H,
+            diag_block,
+            VMaj,
+            idty2L,
+            majorana_indices,
+            gamma,
+            t_max,
+            dt,
+            j,
+            rng
+        )
+
+        S_trajs[:, j] = S
+    end
+
+    mean_S = mean(S_trajs, dims=2)[:]
+    std_S = std(S_trajs, dims=2)[:]
+
+    return times, mean_S, std_S, S_trajs
+end
+
+# Multi-processing calculation of QSD ensemble
+# Single trajectory function for distributed computing
+function single_traj(
+    j, L, exp_H, diag_block, VMaj, idty2L, majorana_indices, gamma, t_max, dt, base_seed
+)
+    try
+        println("Trajectory #$j is running on process $(myid())")
+        rng = Xoshiro(base_seed + j)
+        t, S = runQSD(L, exp_H, diag_block, VMaj, idty2L, majorana_indices, gamma, t_max, dt, j, rng)
+        return S
+    catch e 
+        @warn "Trajectory #$(j) failed on worker $(myid()) with error: $e"
+        return fill(NaN, round(Int, t_max / dt))
+    end
+end
+
+function runQSDEnsembleDistributed(
+    L::Int,
+    ntraj::Int,
+    exp_H,
+    diag_block,
+    VMaj,
+    idty2L,
+    majorana_indices,
+    gamma::Float64,
+    t_max::Float64,
+    dt::Float64,
+    base_seed::Int=1234
+)
+    nsteps = round(Int, t_max / dt)
+    times = collect(1:nsteps) .* dt
+
+    # Run each trajectory on a different worker
+    S_list = pmap(j -> single_traj(
+            j, L, exp_H, diag_block, VMaj, idty2L, majorana_indices, gamma, t_max, dt, base_seed
+        ), 1:ntraj)
+
+    # Collect into an array (each column = one trajectory)
+    S_trajs = hcat(S_list...)  # size: (nsteps, ntraj)
+    mean_S = mean(S_trajs, dims=2)[:]
+    std_S = std(S_trajs, dims=2)[:]
+
+    return times, mean_S, std_S, S_trajs
+end
---
+++ utils.jl
@@ -0,0 +1,139 @@
+using LinearAlgebra, SparseArrays
+using FastExpm
+
+kronDelta(i,j) = (i==j) ? 1.0 : 0.0
+
+# Build the BdG Hamiltonian
+function buildHbdG(J, L)
+    diag1 = ones(L-1)
+    A = spdiagm(1 => ComplexF64(J)*diag1, -1 => ComplexF64(J)*diag1)
+    B = spdiagm(1 => ComplexF64(J)*diag1, -1 => -ComplexF64(J)*diag1)
+    
+    return 0.5 * [-A' -B; B A]
+end
+
+# Initial correlation matrix G(0)
+#   [ <c† c>    <c† c†> ;
+#     <c c>     <c c†> ]
+# and the initial state is |000...0>
+function GInit(L)
+    zeroL = zeros(ComplexF64, L, L)
+    idtyL = Matrix{ComplexF64}(I, L, L)
+    top = hcat(zeroL, zeroL)
+    bot = hcat(zeroL, idtyL)
+    return vcat(top, bot)
+end
+
+# Build the "diag_block" used to calculate G(t) = U * diag_block * U†
+function diagBlockMatrix(L)
+    top = hcat(Matrix{ComplexF64}(I, L, L), zeros(ComplexF64, L, L))
+    bot = hcat(zeros(ComplexF64, L, L), zeros(ComplexF64, L, L))
+
+    return vcat(top, bot)
+end
+
+# Construct U0 from G0
+# To verify just do U0 * diag_block * U0† and see if you get G0
+function buildU0(L)
+    U0 = [zeros(ComplexF64, L, L) Matrix{ComplexF64}(I, L, L);   
+        Matrix{ComplexF64}(I, L, L) zeros(ComplexF64, L, L)]
+
+    return U0
+end
+
+# Majorana transform
+function VMajorana(L)
+    idtyL = Matrix{ComplexF64}(I, L, L)
+    top = hcat(idtyL, idtyL)
+    bot = hcat(1im * idtyL, -1im * idtyL)
+
+    return vcat(top, bot)
+end
+
+function getMajoranaIndices(L, Lsub::Int64)
+    # Majorana indices for sites 1..Lsub 
+    # There are 2 Majorana modes per site
+    maj_indices = Vector{Int}(undef, 2*Lsub)
+    for j in 1:Lsub
+        maj_indices[j] = j
+        maj_indices[Lsub + j] = L + j
+    end
+    
+    return maj_indices
+end
+
+function getEntanglementEntropy(G, idty2L, VMajorana, majorana_indices)
+    Wtilde = getWTilde(G, idty2L, VMajorana)
+    
+    # Sanity check 
+    if any(isnan, Wtilde) || any(isinf, Wtilde)
+        println("NaN/Inf detected in Wtilde — skipping entropy calculation")
+        return 0.0
+    end
+
+    # Restrict Wtilde to the subsystem
+    Wtilde = Wtilde[majorana_indices, majorana_indices]
+
+    # Handling the numerical drift
+    Wtilde .= real.(Wtilde) # Force to be real
+    Wtilde .= 0.5 .* (Wtilde .- Wtilde') # Force to be antisymmetric
+    
+    # Eigenvalues of Wtilde
+    λ_all = try
+            real.(eigvals(im * Wtilde))        
+    catch e 
+        println("eigvals failed to converge: $e — skipping entropy calculation")
+        return 0.0
+    end
+
+    λ_k = filter(x -> x > 0.0, λ_all) # Keep only positive eigenvalues
+
+    # Entanglement entropy 
+    S_ent = 0.0
+
+    for λ in λ_k
+        p = (1 + λ) / 2
+        q = (1 - λ) / 2
+        if p > 0; S_ent -= p * log(p); end
+        if q > 0; S_ent -= q * log(q); end
+    end
+
+    return S_ent
+end
+
+# Prepare computation for QSD
+function prepare(J, L, Lsub, dt)
+    # Build the H_BdG
+    Ising_HBdG = buildHbdG(J, L)
+
+    # Compute the exponentiated H_BdG
+    exp_H = fastExpm(2 * 1im * Ising_HBdG * dt)
+    
+    # diag_block matrix for G(t) = U * diag_block * U†
+    diag_block = diagBlockMatrix(L)
+
+    # Majorana indices
+    maj_indices = getMajoranaIndices(L, Lsub)
+
+    # Majorana transformation matrix
+    VMaj = VMajorana(L)
+
+    idty2L = Matrix{ComplexF64}(I, 2*L, 2*L)
+
+    return exp_H, diag_block, maj_indices, VMaj, idty2L
+
+end
+
+function isComputeS(step, dt)
+    # How many steps to make it 1.0 second?
+    ref_step = Int(round(1.0 / dt))
+
+    # What is the current time?
+    current_time = step * dt
+
+    res = true
+    if current_time > 1.0 && step % ref_step != 0
+        res = false
+    end
+    return res
+end
